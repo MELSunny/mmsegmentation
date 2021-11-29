e953195b4900726d7516704c48209f177186a679
yanwe <yanwenchn@gmail.com>
2021-11-29 15:09:02 +0000

    update deeplabv3 oar-zib-mri

--- configs/deeplabv3plus/deeplabv3plus_r101-d8_512x512_160k_oai_zib_mri.py
+++ configs/deeplabv3plus/deeplabv3plus_r101-d8_512x512_160k_oai_zib_mri.py
@@ -1,5 +1,2 @@
-_base_ = [
-    '../_base_/models/deeplabv3plus_r50-d8.py', '../_base_/datasets/oai_zib_mri.py',
-    '../_base_/default_runtime.py', '../_base_/schedules/schedule_160k.py'
-]
+_base_ = './deeplabv3plus_r50-d8_512x512_160k_oai_zib_mri.py'
 model = dict(pretrained='open-mmlab://resnet101_v1c', backbone=dict(depth=101))
