cf3cd17c72ceb638337e99af38a9bdb3a927b3db
lincoln <yanwenchn@outlook.com>
2022-01-26 03:39:32 +0000

    change 22k to 1k

--- configs/fp16/upernet_swin_base_patch4_window12_512x512_160k_fp16_oai_zib_mri_pretrain_384x384_1K.py
+++ configs/fp16/upernet_swin_base_patch4_window12_512x512_160k_fp16_oai_zib_mri_pretrain_384x384_1K.py
@@ -1,4 +1,4 @@
-_base_ = '../swin/upernet_swin_base_patch4_window12_512x512_160k_oai_zib_mri_pretrain_384x384_22K.py'
+_base_ = '../swin/upernet_swin_base_patch4_window12_512x512_160k_oai_zib_mri_pretrain_384x384_1K.py'
 # fp16 settings
 optimizer_config = dict(type='Fp16OptimizerHook', loss_scale=512.)
 # fp16 placeholder
