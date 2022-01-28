0f19974b8d4ad07b25c977b8274b220962975b87
lincoln <yanwenchn@outlook.com>
2022-01-28 01:07:59 +0000

    add segformer

---
+++ configs/fp16/segformer_mit-b4_512x512_160k_fp16_oai_zib_mri.py.py
@@ -0,0 +1,5 @@
+_base_ = '../segformer/segformer_mit-b4_512x512_160k_oai_zib_mri.py'
+# fp16 settings
+optimizer_config = dict(type='Fp16OptimizerHook', loss_scale=512.)
+# fp16 placeholder
+fp16 = dict()
